f36d60884ea771f1d25ea9759ee865a634228269
Victoriaadame <victoriaadame2468@gmail.com>
2025-06-20 14:41:18 -0700

    h

--- script.js
+++ script.js
@@ -8,15 +8,18 @@ function setup() //called once and program starts
   
 function draw() //This runs in a loop to update the display
 {
-point(150,250);
-line(0,400,400,0); /*
-
+/*point(150,250);*/
+ellipse(mouseX, mouseY, 20, 20); //draws a circle at the mouse position
+line(0,400,400,0); 
+<h1>draw function</h1>
 rect(0,0,100,100);
 rect(100,100,100,100);
 rect(200,200,100,100);
 rect(300,300,100,100);
+
 }
-*/
+
+
 function drawAxes() //draws the axes
 {
    stroke(0);
