4980d7745d3ada3a88aa419983a2f24e15865002
Victoriaadame <victoriaadame2468@gmail.com>
2025-06-20 11:52:28 -0700

    drawing

--- script.js
+++ script.js
@@ -6,5 +6,11 @@ function setup() //called once and program starts
   
 function draw() //This runs in a loop to update the display
 {
+point(150,250);
+line(0,400,400,0);
 
+rect(0,0,100,100);
+rect(100,100,100,100);
+rect(200,200,100,100);
+rect(300,300,100,100);
 }
